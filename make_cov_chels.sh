4becd56c98e77f736812bd19c1a5f6d06e98a2c5
caseyalananderson <casey.alan.anderson@gmail.com>
2017-10-30 22:23:04 -0600

    added mod cov letter using naming _chels

---
+++ make_cov_chels.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+res='resume_main.tex'
+bcf='resume_main.bcf'
+pdf='resume_main.pdf'
+xe='xelatex'
+bi='biber'
+
+irc='resume_dangelo.pdf'
+
+ref='references_main.tex'
+ref_long='references_long_main.tex'
+cov='coverletter_main_chels.tex'
+covpdf='coverletter_main_chels.pdf'
+
+# comiple resume
+#$xe $res
+#$bi $bcf
+#$xe $res
+
+# compile references
+#$xe $ref
+#$xe $ref
+
+# compile references long
+#$xe $ref_long
+#$xe $ref_long
+
+# compile cover letter
+$xe $cov
+$bi $bcf
+$xe $cov
+
+gnome-open $covpdf
+
+#cp $pdf $irc
+
+#gnome-open $irc &
